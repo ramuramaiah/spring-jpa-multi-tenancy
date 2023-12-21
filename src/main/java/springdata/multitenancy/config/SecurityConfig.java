b5d8f90d96566b2f9187e5536d242e1d0e8051c4
Ramu Ramaiah <rara@softwareag.com>
2023-12-21 09:56:54 +0530

    upgrade to spring boot version 2.7.12

--- src/main/java/springdata/multitenancy/config/SecurityConfig.java
+++ src/main/java/springdata/multitenancy/config/SecurityConfig.java
@@ -25,8 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/**").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/**").permitAll()
                 .and()
                 .httpBasic()
                 .and()
